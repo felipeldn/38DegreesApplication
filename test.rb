cb0bd57063aaf18bf4f49082478a4d8fc0e68a9e
felipeldn91 <felipeldn91@gmail.com>
2021-10-04 21:04:14 +0100

    Notes

--- test.rb
+++ test.rb
@@ -1,3 +1,9 @@
+# Print numbers 1 to 100 on separate lines 
+# Print 'People' for numbers divisble by 3
+# Print 'power' for numbers divisble by 5
+# Print 'People power' if divisble by both 3 and 5
+
+
 def peoplePower
     
     (1..100).each {|n| puts n}
