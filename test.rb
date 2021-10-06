10e55ee8bd8e34c13778ef6bec917a4006efdc75
felipeldn91 <felipeldn91@gmail.com>
2021-10-06 14:11:42 +0100

    divisible by 3 and 5 working

--- test.rb
+++ test.rb
@@ -11,7 +11,7 @@ def peoplePower
     numbers.each {|n| 
        
         if n % 3 == 0 && n % 5 == 0
-            puts "People power!"
+            puts "People power"
         elsif n % 3 == 0 
             puts "People"
         elsif n % 5 == 0
